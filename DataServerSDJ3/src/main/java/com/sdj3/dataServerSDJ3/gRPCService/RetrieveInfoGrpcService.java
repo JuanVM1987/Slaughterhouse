59f51332d98925c8ecee840a962e93bb9e65a803
Juan <315208@via.dk>
2022-11-20 18:53:57 +0100

    update

--- DataServerSDJ3/src/main/java/com/sdj3/dataServerSDJ3/gRPCService/RetrieveInfoGrpcService.java
+++ DataServerSDJ3/src/main/java/com/sdj3/dataServerSDJ3/gRPCService/RetrieveInfoGrpcService.java
@@ -7,19 +7,23 @@ import com.sdj3.dataServerSDJ3.DAOs.TrayDAO;
 
 import com.sdj3.dataServerSDJ3.protobuf.retrieveInfoProtobuf.*;
 import com.sdj3.dataServerSDJ3.share.entity.Animal;
+import com.sdj3.dataServerSDJ3.share.entity.Part;
 import com.sdj3.dataServerSDJ3.share.entity.Product;
+import com.sdj3.dataServerSDJ3.share.entity.Tray;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @GRpcService
 public class RetrieveInfoGrpcService extends RetrieveServiceGrpc.RetrieveServiceImplBase {
 
     private final ProductDAO productDAO;
     private final PartDAO partDAO;
-    private TrayDAO trayDAO;
+    private final TrayDAO trayDAO;
     private final AnimalDAO animalDAO;
 
 
@@ -81,6 +85,53 @@ public class RetrieveInfoGrpcService extends RetrieveServiceGrpc.RetrieveService
 
     @Override
     public void getAnimalById(AnimalIdMessage request, StreamObserver<AnimalMessage> responseObserver) {
-        super.getAnimalById(request, responseObserver);
+        int animalId = request.getId();
+         Optional<Animal> animal=animalDAO.findById(animalId);
+
+         if (animal.isEmpty()){
+             Status status = Status.FAILED_PRECONDITION.withDescription("Animal id : "+animalId+", not found");
+             responseObserver.onError(status.asRuntimeException());
+             return;
+         }
+         AnimalMessage reply = AnimalMessage.newBuilder().setId(animal.get().getId())
+                 .setOrigen(animal.get().getOrigen()).setArrivedDate(String.valueOf(animal.get().getArrivedDate())).build();
+
+         responseObserver.onNext(reply);
+         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllTrays(EmptyMessage request, StreamObserver<ListTray> responseObserver) {
+        List<Tray> trayList = trayDAO.findAll();
+        ListTray.Builder builder = ListTray.newBuilder();
+        for (Tray t:trayList) {
+            builder.addTrayMessage(TrayMessage.newBuilder().setId(t.getId()).setName(t.getName())
+                    .setMaxWeight(t.getMax_weight()));
+        }
+
+        ListTray reply= builder.build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+
+    }
+
+    @Override
+    public void getAllPartsInTray(TrayIdMessage request, StreamObserver<ListParts> responseObserver) {
+        int tryId= request.getId();
+        Optional<Tray> tray = trayDAO.findById(tryId);
+        if (tray.isEmpty()){
+            Status status = Status.FAILED_PRECONDITION.withDescription("Tray id : "+tryId+", not found");
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+        List<Part> partList = partDAO.getAllPartsInTray(tryId);
+        ListParts.Builder builder = ListParts.newBuilder();
+        for (Part p:partList) {
+            builder.addPartMessage(PartMessage.newBuilder().setPartId(p.getId())
+                    .setName(p.getName()).setWeight(p.getWeight()));
+        }
+        ListParts reply =builder.build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
     }
 }
